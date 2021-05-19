7b10f9783b8dbff9b18e86ae86c719f36c3e0367
javarex <raymartnayreitanong11@gmail.com>
2021-05-19 08:21:51 +0800

    update division acronym

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -37,25 +37,28 @@ class ImportController extends Controller
                     if ($rowIndex === 1) {
                         continue;
                     } else {
-                        $data_division = [
-                            'id'            => $division_id,
-                            'office_id'       => $office_id,
-                            'division'       => $division,
-                            'acronym'       => $acronym,
-                            'created_at'    => $dateTime,
-                            'updated_at'    => $dateTime
+                        DB::table('divisions')
+                            ->where('id', $division_id)
+                            ->update(['acronym' => $acronym]);
+                        // $data_division = [
+                        //     'id'            => $division_id,
+                        //     'office_id'       => $office_id,
+                        //     'division'       => $division,
+                        //     'acronym'       => $acronym,
+                        //     'created_at'    => $dateTime,
+                        //     'updated_at'    => $dateTime
                             
-                        ];
+                        // ];
                     }
-                    array_push($division_array,$data_division);
+                    // array_push($division_array,$data_division);
                 }
             }
             $reader->close();
-            $chunk_division = array_chunk($division_array, 15);
+            // $chunk_division = array_chunk($division_array, 15);
 
-            foreach ($chunk_division as $key => $value) {
-                DB::table('divisions')->insert($value);
-            }
+            // foreach ($chunk_division as $key => $value) {
+            //     DB::table('divisions')->insert($value);
+            // }
             Storage::disk('public')->delete('file.xlsx');
         }
 
